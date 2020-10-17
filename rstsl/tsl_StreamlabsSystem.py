a537543ef7d2c42b05614d6b27a22a90999b0c2f
Kirin Patel <kirinpatel@gmail.com>
2020-10-17 00:54:19 -0400

    Created RSTSL script directory

---
+++ rstsl/tsl_StreamlabsSystem.py
@@ -0,0 +1,10 @@
+ScriptName = "Rocksmith Link Song Request"
+Website = "https://github.com/ajchili/rslsr"
+Description = "Rocksmith song request bot for Spotify links"
+Creator = "ajchili (Kirin Patel)"
+Version = "0.1.0"
+
+configFile = "config.json"
+settings = {}
+
+# TODO: Write script
